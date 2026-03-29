feca65c88b97a9dc3e73032bcd0f0c49b2db5e9b
kshitijdev24 <kshitij.chetan030174@gmail.com>
2026-03-30 02:39:50 +0530

    feat(eventhandling-demo): enhance event handling with dynamic background color

--- src/app/eventhandling-demo/eventhandling-demo.ts
+++ src/app/eventhandling-demo/eventhandling-demo.ts
@@ -1,16 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-eventhandling-demo',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './eventhandling-demo.html',
   styleUrl: './eventhandling-demo.css',
 })
 export class EventhandlingDemo {
   count: number = 0;
+  bgColor = 'white';
   incrementCount(event: any) {
     this.count += 1;
     console.log(event);
     console.log('Button clicked! Count: ' + event.target.value);
   }
+  changeBackground() {
+    this.bgColor = 'pink';
+  }
+
 }
